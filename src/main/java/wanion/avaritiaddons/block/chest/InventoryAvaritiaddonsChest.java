9443f5807c14037c485747b7a135a5c5a12fe652
GitHub GTNH Actions <>
2026-08-14 15:54:06 +0000

    spotlessApply

--- src/main/java/wanion/avaritiaddons/block/chest/InventoryAvaritiaddonsChest.java
+++ src/main/java/wanion/avaritiaddons/block/chest/InventoryAvaritiaddonsChest.java
@@ -11,7 +11,6 @@ import javax.annotation.Nonnull;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.MathHelper;
 
 public final class InventoryAvaritiaddonsChest implements IInventory {
 
